3823190137dee8d9e57fc1246b489f602ba4b693
quitd <degrou99@gmail.com>
2022-03-04 17:27:39 +0700

    test

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fetch = require('cross-fetch');
 app.lib.cron(e => {
   fetch('https://api.github.com/repos/quitd/balloons-be/actions/workflows/21097005/dispatches', {
     method: 'post',
+    body: JSON.stringify({ref: 'master'}),
     headers: {
       Authorization: 'token '+process.env.GH
     }
