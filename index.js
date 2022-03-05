04169aa7ea2a8ab8e089dd91001746e48fdaf4a2
quitd <degrou99@gmail.com>
2022-03-05 10:06:10 +0700

    rescan

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const { app } = require('deta');
+const { App } = require('deta');
 const fetch = require('cross-fetch');
+const express = require('express');
+const app = App(express());
+
+app.all('/', (_, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  f();
+  res.end()
+})
 
 app.lib.cron(e => {
+  f();
+})
+
+function f() {
   fetch('https://api.github.com/repos/quitd/balloons-be/actions/workflows/21097005/dispatches', {
     method: 'post',
     body: JSON.stringify({ref: 'master'}),
@@ -9,6 +21,6 @@ app.lib.cron(e => {
       Authorization: 'token '+process.env.GH
     }
   }).then(g => {if(!g.ok) console.log(g)})
-})
+}
 
 module.exports = app;
